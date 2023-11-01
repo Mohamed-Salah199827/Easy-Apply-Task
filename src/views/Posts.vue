2b9ee90b7666bbd6dc808daa8d07103d0717e5f8
mohamed72sa <m7mdslah45@gmail.com>
2023-11-01 09:41:25 +0200

    remove card

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -31,7 +31,6 @@ onBeforeMount(() => {
 </script>
 
 <template>
-  <Card  :post="Post" />
   <div class="row row-cols-1 row-cols-md-3 g-4">
     <template v-for="post in PostsStore.Posts[currentPage]" :key="post.id">
       <div
